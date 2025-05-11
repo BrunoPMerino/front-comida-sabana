86c4f9c848bf76814adb94948f8eac910c9acfa5
Diego Escalante <descalanteg05@gmail.com>
2025-05-11 15:21:57 -0500

    Integrated back and front as a whole

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,36 @@
 import { useState } from "react";
+import axios from "axios";
 import LogoHeader from "../components/LogoHeader";
 import AuthCard from "../components/AuthCard";
 import FormInput from "../components/FormInput";
 import PrimaryButton from "../components/PrimaryButton";
 import { Link } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
+import { useUserStore } from "../store/useUserStore"
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const API_URL = import.meta.env.VITE_API_URL;
+  //const navigate = useNavigate();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`${API_URL}/api/auth/login`, { email, password }, {withCredentials: true,});
+      const meResponse = await axios.get(`${API_URL}/api/auth/me`, {withCredentials: true,});
+      const user = meResponse.data.user;
+      useUserStore.setUser(user); // saves in Zustand
+      //navigate("/dashboard");
+    } catch (error) {
+      console.error("Login failed: ", error.response?.data || error.message);
+    }
+  };
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center px-4 bg-white">
       <LogoHeader />
       <AuthCard title="Inicio de sesión">
-        <form>
+        <form> //onSubmit?
           <FormInput
             label="Correo electrónico"
             type="email"
@@ -44,7 +61,7 @@ export default function Login() {
                 </Link>
             </div>
 
-          <PrimaryButton>Ingresar</PrimaryButton>
+          <PrimaryButton onClick={handleLogin}>Ingresar</PrimaryButton> //maybe change button on click functionality
         </form>
       </AuthCard>
     </div>
